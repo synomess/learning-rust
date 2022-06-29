268b7517667b2c1bc560a2cd7703066e22e15d7a
synomess <synomess@gmail.com>
2022-06-29 16:05:12 +0300

    duzenleme

--- src/bin/a3a.rs
+++ src/bin/a3a.rs
@@ -1,3 +1,12 @@
+fn main() {
+    let allah_var:bool=true;
+    if allah_var== false {
+        println!("NE DION MAL");
+    }else {
+        println!("ALLAHU AKBAR");
+    }
+}
+
 // Topic: Flow control using if..else
 //
 // Program requirements:
@@ -9,12 +18,3 @@
 // * Use a variable set to either true or false
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
-
-fn main() {
-    let allah_var:bool=true;
-    if allah_var== false {
-        println!("NE DION MAL");
-    }else {
-        println!("ALLAHU AKBAR");
-    }
-}
