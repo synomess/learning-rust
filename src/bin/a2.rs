499ede7b95851aa081ff19fa066846981c43c4ae
synomess <synomess@gmail.com>
2022-06-29 15:58:58 +0300

    duzenleme

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -1,13 +1,3 @@
-// Topic: Basic arithmetic
-//
-// Program requirements:
-// * Displays the result of the sum of two numbers
-//
-// Notes:
-// * Use a function to add two numbers together
-// * Use a function to display the result
-// * Use the "{:?}" token in the println macro to display the result
-
 fn main() {
     let  a:i32=24;
     let  b:i32=1;
@@ -23,3 +13,14 @@ fn main() {
     }
     else{ println!("{}",ab=ab+2);}
     }fn abc(a:i32,b:i32)->i32 {a+b}
+    
+    // Topic: Basic arithmetic
+//
+// Program requirements:
+// * Displays the result of the sum of two numbers
+//
+// Notes:
+// * Use a function to add two numbers together
+// * Use a function to display the result
+// * Use the "{:?}" token in the println macro to display the result
+
