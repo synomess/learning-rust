9c61c5b6bea06e7ebebe9f41d274c652dd7df168
synomess <synomess@gmail.com>
2022-06-29 15:57:03 +0300

    deneme

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -1,10 +1,1 @@
-fn main() {
-    let name:&str="Saydemr";
-    match name {
-        "Saydemr" => println!("yasliya yer verin"),
-        "Recep" => println!("Erdugan geldi"),
-        "Sude" => println!("selam hatice"),
-        _=> println!("selam kardes hos geldin"),
-
-    }
-}
+fn main() {}
